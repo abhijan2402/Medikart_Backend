84314579a0e43844dbd3a5d48d98d2502975ea27
DSRCODE <daljeetkalyanwat62@gmail.com>
2025-11-03 18:54:56 +0530

    code

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ app.use(cookieParser());
 app.use(express.static("uploads"));
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL, // http://localhost:3001
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions)); 
+// app.options("*", cors(corsOptions));
 
 /* ---------- DB ---------- */
 mongoose.set("strictQuery", false);
